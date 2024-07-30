4021081f31622531e8ce78c3f06c3d77deef22e8
Kevin Garner <kevinleegarner@gmail.com>
2024-07-29 22:32:05 -0500

    Removed outdated explanation on Tab Selection.

--- src/applications/tab-selection/TabSelection.svelte
+++ src/applications/tab-selection/TabSelection.svelte
@@ -4,13 +4,17 @@
   import type { Writable } from 'svelte/store';
   import type { TabSelectionContext } from './TabSelectionFormApplication';
   import { FoundryAdapter } from 'src/foundry/foundry-adapter';
-    import { CONSTANTS } from 'src/constants';
+  import { CONSTANTS } from 'src/constants';
 
-  let context = getContext<Writable<TabSelectionContext>>(CONSTANTS.SVELTE_CONTEXT.CONTEXT);
+  let context = getContext<Writable<TabSelectionContext>>(
+    CONSTANTS.SVELTE_CONTEXT.CONTEXT,
+  );
 
   const apply = getContext<() => Promise<void>>(CONSTANTS.SVELTE_CONTEXT.APPLY);
   const validate = getContext<() => boolean>(CONSTANTS.SVELTE_CONTEXT.VALIDATE);
-  const useDefault = getContext<() => Promise<void>>(CONSTANTS.SVELTE_CONTEXT.USE_DEFAULT);
+  const useDefault = getContext<() => Promise<void>>(
+    CONSTANTS.SVELTE_CONTEXT.USE_DEFAULT,
+  );
   const localize = FoundryAdapter.localize;
 </script>
 
@@ -30,12 +34,6 @@
       {localize('TIDY5E.TabSelection.SelectedHeader')}
     </h2>
   </SelectionListbox>
-  <p class="flex-row align-items-center no-gap">
-    <span class="extra-info-icon-container"
-      ><i class="fas fa-info-circle"></i></span
-    >
-    <span>{localize('TIDY5E.TabSelection.Explanation')}</span>
-  </p>
   <div class="flex-row small-gap">
     <button
       type="button"
